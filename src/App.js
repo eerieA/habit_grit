e61ed849859a3f346705bacb615fc2ccedabf7e4
eerieA <ninja.duan@gmail.com>
2024-01-30 02:08:20 -0800

    Basic supabase calling code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { useState, useEffect } from 'react';
+import supabase from "./supabase.js";
+
 import logo from './logo.svg';
 import './App.css';
-import supabase from './supabase'
+
 
 function App() {
+
+  const [hdscrs, setHabits] = useState([]);
+
+  useEffect(
+    function () {
+    // Function to fetch all IDs from the "Habits" table
+    async function fetchHids() {
+      try {
+        // Fetch all rows from Habits table
+        let { data: habits, error } = await supabase.from("Habits").select("*");
+
+        if (error) {
+          console.error('Error fetching habits from Supabase:', error);
+          return;
+        }
+
+        // Extract the IDs from the response data
+        console.log('habits:', habits);
+        const hdscrs = habits.map(habit => habit.HDscr);
+        setHabits(hdscrs);
+        
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    // Call the function to fetch item IDs
+    fetchHids();
+  }, []); // Run the effect only once when the component mounts
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +44,11 @@ function App() {
       <h1>
         Hello world.
       </h1>
+      <ul>
+        {hdscrs.map(hdscr => (
+          <li>{hdscr}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import config from './.env.json'
 
+// This key has to be service key, not public/anon key
 const supabase = createClient(config.Supa_url, config.Supa_Key);
 
 export default supabase;
